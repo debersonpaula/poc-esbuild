b9afb13adcb41e2cfc98289d3605a8908da8cd8b
Deberson de Paula <deberson.paula@gmail.com>
2021-07-10 13:40:15 -0300

    added dev-server

--- build.js
+++ build.js
@@ -1,23 +1,23 @@
-const fs = require("fs");
-const path = require("path");
-const root = __dirname;
-const output = "dist";
-const template = "src/index.html";
+// const fs = require("fs");
+// const path = require("path");
+// const root = __dirname;
+// const output = "dist";
+// const template = "src/index.html";
 
-const outputPath = path.resolve(root, output);
-if (!fs.existsSync(outputPath)) {
-  console.log("Creating output path: " + outputPath);
-  fs.mkdirSync(outputPath, { recursive: true });
-}
+// const outputPath = path.resolve(root, output);
+// if (!fs.existsSync(outputPath)) {
+//   console.log("Creating output path: " + outputPath);
+//   fs.mkdirSync(outputPath, { recursive: true });
+// }
 
-const inputConfigFile = path.resolve(root, template);
-const outputConfigFile = path.resolve(outputPath, "index.html");
-if (fs.existsSync(inputConfigFile)) {
-  fs.copyFileSync(inputConfigFile, outputConfigFile);
-} else {
-  console.error(`Config file ${inputConfigFile} not found`);
-  process.exit(1);
-}
+// const inputConfigFile = path.resolve(root, template);
+// const outputConfigFile = path.resolve(outputPath, "index.html");
+// if (fs.existsSync(inputConfigFile)) {
+//   fs.copyFileSync(inputConfigFile, outputConfigFile);
+// } else {
+//   console.error(`Config file ${inputConfigFile} not found`);
+//   process.exit(1);
+// }
 
 require("esbuild")
   .build({
